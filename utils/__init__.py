437b1e82fc1c0a4723d934548b3ebf6027cfea50
SamithVA <vasamith@163.com>
2025-11-30 02:31:28 +0800

    remove unused plot_training_history import from utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,7 +6,6 @@ from .dataset import ChineseSegDataset, collate_fn, build_vocab
 from .trainer import train_epoch, evaluate, train_model
 from .tokenizer import BaseTokenizer
 from .evaluation import evaluate_segmentation, evaluate_model_on_all_datasets
-from .visualization import plot_training_history
 
 __all__ = [
     'ChineseSegDataset',
@@ -17,6 +16,5 @@ __all__ = [
     'train_model',
     'BaseTokenizer',
     'evaluate_segmentation',
-    'plot_training_history',
     'evaluate_model_on_all_datasets'
 ]
